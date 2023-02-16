691d034d0fb635852d916fa39ab88dd52d6601ff
insert github real name here~ <125500278+Ricardoguima@users.noreply.github.com>
2023-02-16 14:52:44 -0800

    _

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gununicorn First-app.wsgi --log-file -
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,19 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+if os.getcwd() == '/app':
+    import dj_database_url
+    db_from_venv = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_venv)
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    ALLOWED_HOSTS = ['First-app.herokuapp.com']
+    DEBUG = True
+    
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
